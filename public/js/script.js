9d4390db49260eb5b97cf2918b77f1b206a7e542
Nikita Kapoor <nikitakapoor1998@gmail.com>
2020-04-01 17:01:39 +0530

    updated

--- public/js/script.js
+++ public/js/script.js
@@ -7,20 +7,41 @@
 
  
 $(document).ready(function(){
-    $("#heart").click(function(){
-      if($("#heart").hasClass("liked")){
-       $("#heart").html('<i class="fa fa-h-o" aria-hidden="true"></i>');
-        $("#heart").removeClass("liked");
+    $("this").click(function(){
+      if($("this").hasClass("liked")){
+       $("this").html('<i class="fa fa-h-o" aria-hidden="true"></i>');
+        $("this").removeClass("liked");
       localStorage.setItem('class', 'fa fa-heart-o'); 
       }else{
-        $("#heart").html('<i class="fa fa-h" aria-hidden="true"></i>');
-        $("#heart").addClass("liked");
+        $("this").html('<i class="fa fa-h" aria-hidden="true"></i>');
+        $("this").addClass("liked");
       localStorage.setItem('class', 'fa fa-heart'); 
       }
     }
     );
   });
- 
+   $('.heart').click(function(e){
+   e.preventDefault()
+   var this_=$(this)
+   var id=this_.attr('like-id')
+   var URL='/user/like/'+id
+   $.ajax({
+     url:URL,
+     method:'GET',
+     data:{},
+     success:function(data){
+      if(this_.hasClass("liked")){
+       this_.html('<i class="fa fa-heart-o" aria-hidden="true"></i>');
+         this_.removeClass("liked");
+         localStorage.setItem('class', 'fa fa-heart-o'); 
+       }else{
+         this_.html('<i class="fa fa-heart" aria-hidden="true"></i>');
+         this_.addClass("liked");
+         localStorage.setItem('class', 'fa fa-heart'); 
+       }
+     }
+   })
+ })
 
    */ 
  
