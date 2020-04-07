8c6697b328c8452ef363cb4c6058fbd1f100d724
Nikita Kapoor <nikitakapoor1919@gmail.com>
2020-04-07 15:37:31 +0530

    like button updated

--- public/js/script.js
+++ public/js/script.js
@@ -4,22 +4,8 @@
      $('#h').removeClass()
      $('#h').addClass(retrievedObject)
  }
-
- 
-$(document).ready(function(){
-    $("this").click(function(){
-      if($("this").hasClass("liked")){
-       $("this").html('<i class="fa fa-h-o" aria-hidden="true"></i>');
-        $("this").removeClass("liked");
-      localStorage.setItem('class', 'fa fa-heart-o'); 
-      }else{
-        $("this").html('<i class="fa fa-h" aria-hidden="true"></i>');
-        $("this").addClass("liked");
-      localStorage.setItem('class', 'fa fa-heart'); 
-      }
-    }
-    );
-  });
+<script>
+  var response=" "
    $('.heart').click(function(e){
    e.preventDefault()
    var this_=$(this)
@@ -30,18 +16,40 @@ $(document).ready(function(){
      method:'GET',
      data:{},
      success:function(data){
-      if(this_.hasClass("liked")){
-       this_.html('<i class="fa fa-heart-o" aria-hidden="true"></i>');
-         this_.removeClass("liked");
-         localStorage.setItem('class', 'fa fa-heart-o'); 
-       }else{
-         this_.html('<i class="fa fa-heart" aria-hidden="true"></i>');
-         this_.addClass("liked");
-         localStorage.setItem('class', 'fa fa-heart'); 
-       }
+       response=data
+      this_.text(data.likeCount);
+      if(response.text=="liked"){
+        this_.removeClass();
+        this_.addClass("fa fa-heart heart")
+        this_.append("<div>You like this Post</div>")
+         localStorage.setItem('class', 'fa fa-heart heart'); 
+      }
+      else{
+         this_.removeClass();
+         this_.addClass("fa fa-heart-o heart")
+        localStorage.setItem('class', 'fa fa-heart heart'); 
+      }
      }
    })
  })
 
+</script>
    */ 
  
+  var response=" "
+   $('.heart').click(function(e){
+   e.preventDefault()
+   var this_=$(this)
+   var id=this_.attr('like-id')
+   var URL='/user/like/'+id
+   $.ajax({
+     url:URL,
+     method:'GET',
+     data:{},
+     success:function(data){
+       response=data
+      this_.text(data.likeCount);
+      
+     }
+   })
+ })
