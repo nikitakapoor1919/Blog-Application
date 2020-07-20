6ef373f49832e26654efdd1afdecace79ebc7189
Nikita Kapoor <nikitakapoor1919@gmail.com>
2020-07-20 21:07:37 +0530

    changed url to window.location.href

--- routes/user.js
+++ routes/user.js
@@ -49,6 +49,7 @@ router.post('/add-post',isLoggedIn,upload.single('image'), function(req,res){
 
 //Add comment
 router.get('/add-comment/:id',isLoggedIn, function(req, res, next){
+
   Post.find({_id:req.params.id})
   .exec( function(err, post){
     var postChunk = []; var chunkSize = 3;
